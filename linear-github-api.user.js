56a67949e7484187821b97e27dfbb347101276ab
Francesco <frosso@users.noreply.github.com>
2026-02-09 09:33:00 +0100

    Modify PR title selector to support self-hosted GitHub

--- linear-github-api.user.js
+++ linear-github-api.user.js
@@ -1007,7 +1007,8 @@
     }
 
     function getPRTitle() {
-        const titleElement = document.querySelector('h1[class*="prc-PageHeader-Title-"] > span');
+        // the H1 is for github.com, the `.js-issue-title` is for the self-hosted github.a8c.com
+        const titleElement = document.querySelector('h1[class*="prc-PageHeader-Title-"] > span,.js-issue-title');
         return titleElement ? titleElement.textContent.trim() : '';
     }
 
